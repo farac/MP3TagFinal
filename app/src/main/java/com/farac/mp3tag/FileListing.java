ad0d21c1e18fba8b3dde7f08e0fba946ee21aeed
bruno <brunofarac@gmail.com>
2017-02-01 11:38:46 +0100

    ver1

---
+++ app/src/main/java/com/farac/mp3tag/FileListing.java
@@ -0,0 +1,36 @@
+package com.farac.mp3tag;
+
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FileListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_listing);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public static Intent newIntent(Context packageContext) {
+        Intent i= new Intent(packageContext, FileListing.class);
+        return i;
+    }
+
+}
--- app/src/main/java/com/farac/mp3tag/MainMenu.java
+++ app/src/main/java/com/farac/mp3tag/MainMenu.java
@@ -1,13 +1,30 @@
 package com.farac.mp3tag;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button mButtonEditTags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+        mButtonEditTags= (Button) findViewById(R.id.button_edit_files);
+        mButtonEditTags.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = FileListing.newIntent(MainMenu.this);
+                startActivity (i);
+            }
+        });
     }
 }
+
+
